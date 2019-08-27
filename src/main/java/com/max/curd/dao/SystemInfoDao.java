80dc60f55d7de54b1068e7fb2a639f24239f5ee0
myzou <944711140@qq.com>
2019-08-27 14:25:28 +0800

    2019-08-27 luke Initial commit Spring boot + vue + ElementUI项目 jdk最好1.8 数据库:mysql（可以自行更换） 用到了springbootjpa 做中间层

---
+++ src/main/java/com/max/curd/dao/SystemInfoDao.java
@@ -0,0 +1,16 @@
+package com.max.curd.dao;
+
+import com.max.curd.model.SystemInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 系统信息数据操作类
+ * @author Max
+ * @date 2019-04-10 下午 9:08
+ * @projectName curd
+ */
+@Repository
+public interface SystemInfoDao extends JpaRepository<SystemInfo,String>, JpaSpecificationExecutor<SystemInfo> {
+}
